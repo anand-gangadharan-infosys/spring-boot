34257dd61b3160618823db0e27a932df5a2fbb61
Anand Gangadharan <anand_gangadharan@infosys.com>
2017-07-10 16:42:49 +0530

    Added redis based message processor for delayed db operations

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,59 @@
 package hello;
 
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+	
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("jobs"));
+
+		return container;
+	}
+	@Bean
+	MessageListenerAdapter listenerAdapter(RedisJobReceiverAndProcessor receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+	
+	@Bean
+	RedisJobReceiverAndProcessor receiver(CountDownLatch latch) {
+		return new RedisJobReceiverAndProcessor(latch);
+	}
+
+	@Bean
+	CountDownLatch latch() {
+		return new CountDownLatch(1);
+	}
+
+	@Bean
+	StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
+	}
+
+	
+    public static void main(String[] args) throws InterruptedException {
+    	ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    	
+		//System.exit(0);
     }
 }
